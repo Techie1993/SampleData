dc72baee423f1cb94ff482fa538958eba961086a
Techie1993 <akshayparhad21@gmail.com>
2026-09-21 19:13:17 +0530

    second changes done

--- src/com/jtc/Hackerearth/PrimeData.java
+++ src/com/jtc/Hackerearth/PrimeData.java
@@ -1,11 +1,20 @@
 package com.jtc.Hackerearth;
 
+import java.util.stream.IntStream;
+
 public class PrimeData {
 
 	public static void main(String[] args) {
 		
 		
+		int num=31;
 		
+		
+		
+		boolean check=num>1 && IntStream.rangeClosed(2,(int)(Math.sqrt(num)))
+				.noneMatch(i->num%i==0);
+		
+		System.out.println(check);
 		
 		
 		
